c5c0a7bbd8e1f0ec7f89df33bac44af6b21d79b7
holgerd77 <Holger.Drewes@googlemail.com>
2012-10-12 16:04:00 +0200

    Added admin url to urls.py

--- ber_watch/urls.py
+++ ber_watch/urls.py
@@ -1,10 +1,14 @@
 from django.conf import settings
-from django.conf.urls import patterns, url
+from django.conf.urls import include, patterns, url
+
+from django.contrib import admin
+admin.autodiscover()
 
 from big_projects_watch.urls import urlpatterns
 
 if settings.DEBUG:
     urlpatterns += patterns('',
+        url(r'^admin/', include(admin.site.urls)),
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
         }),
