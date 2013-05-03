97a574c2d41ceaf77ed20a03a8672afd976d9d9b
holgerd77 <Holger.Drewes@googlemail.com>
2013-05-03 23:35:31 +0200

    Added tastypie to settings.py INSTALLED_APPS

--- ber_watch/settings.py
+++ ber_watch/settings.py
@@ -114,6 +114,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'south',
+    'tastypie',
     'kombu.transport.django',
     'djcelery',
     'djcelery_email',
